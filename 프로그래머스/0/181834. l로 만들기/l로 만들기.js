6d630a8f966416f65b85d6e168806637cf7e1142
huiseon Jeon <92114270+heesun2@users.noreply.github.com>
2024-10-05 17:58:58 +0900

    [level 0] Title: l로 만들기, Time: 15.56 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,10 +1,3 @@
 function solution(myString) {
-    en = ['a','b','c','d','e','f','g','h','i','j','k']
-    
-    const result = en.reduce((acc, v) => {
-        return acc.includes(v) ? acc.replaceAll(v, 'l') : acc
-    },myString )
-    
-    return result
+    return [...myString].map((v) => v < 'l' ? 'l' : v).join('')
 }
-   
