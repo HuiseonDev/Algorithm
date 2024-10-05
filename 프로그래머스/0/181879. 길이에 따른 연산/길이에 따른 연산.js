a1e8758e06b0597d1694694b56db592bf10b2518
huiseon Jeon <92114270+heesun2@users.noreply.github.com>
2024-10-05 23:56:52 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,5 @@
+function solution(num_list) {
+   return num_list.length >= 11 
+                ? num_list.reduce((acc, v) => acc + v ,0)
+                : num_list.reduce((acc, v) => acc * v ,1 )
+}
