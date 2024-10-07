e4c90521886df909d9df253e4c9c355b2092352e
huiseon Jeon <92114270+heesun2@users.noreply.github.com>
2024-10-08 01:49:44 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,6 +1,4 @@
 function solution(num_list) {
-    arr= []
-   num_list.map((v, i) => v<0 ? arr.push(i) : '')
-    return arr.length > 0 ? arr[0] : -1
+    return num_list.findIndex(v => v < 0)
    
 }
