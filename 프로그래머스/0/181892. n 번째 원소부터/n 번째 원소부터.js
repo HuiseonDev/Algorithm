6a5c140365733984569f3ba2af401da60297cf69
huiseon Jeon <92114270+heesun2@users.noreply.github.com>
2024-10-09 19:35:28 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -0,0 +1,9 @@
+function solution(num_list, n) {
+    
+    
+    //num_list와 n이 주어졌을 때 num_list의 n번째 부터 return 해주기
+    //num_list의 n번째부터 잘라서 출력
+    let answer = num_list.slice(n-1)
+    
+    return answer;
+}
