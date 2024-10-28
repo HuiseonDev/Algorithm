c5bd22b8ac3029a4c0cb342270e5551a27f5e6e3
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2024-10-28 20:21:19 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,17 +1,10 @@
 function solution(a, b, c) {
     
-    if( a==b && a==c){
-        console.log('모두같음')
-        return (a+b+c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
-        
-    }else if ( a==b || a==c || b==c ){
-        console.log('모두다름')
-        return (a+b+c) * (a**2 + b**2 + c**2)
-        
-    }else{
-        console.log('하나만 다름')
-        return a+b+c
-        
-    }
+        return ( a==b && a==c) 
+                ? (a+b+c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+                : ( a==b || a==c || b==c )
+                ? (a+b+c) * (a**2 + b**2 + c**2)
+                : a+ b+ c
+    
     
 }
