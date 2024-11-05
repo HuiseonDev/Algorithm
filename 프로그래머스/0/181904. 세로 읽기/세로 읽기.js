f18c36bd936e5e9980ae7aa243b5aea2bfdc35ad
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2024-11-05 17:56:52 +0900

    [level 0] Title: 세로 읽기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,10 +1,4 @@
 function solution(my_string, m, c) {
-    const newarr = my_string.split('').reduce((acc,cur,index) => {
-        index % m === 0 ? acc.push([cur]) : acc[acc.length - 1].push(cur)
-        return acc
-    },[])
-    return newarr[0].length === 1 
-        ? my_string 
-        : (newarr.map((v) => v[c - 1])).join('')
+   return [...my_string].filter((_,i) => i % m === c - 1).join('')
 
 }
