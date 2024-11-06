7438cd63bc8c0a0239b4f36b2b4efb0377b0b79c
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2024-11-06 12:36:23 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,9 +1,4 @@
 function solution(str1, str2) {
-    arr = []
-    for(i=0; i<str1.length; i++){
-        arr.push(str1[i])
-        arr.push(str2[i])
-    }
-    return arr.join('')
+    return [...str1].map((v,i) =>(v + str2[i])).join('')
     
 }
