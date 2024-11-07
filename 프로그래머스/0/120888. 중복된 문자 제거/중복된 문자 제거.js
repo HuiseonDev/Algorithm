0edef1d0db8f23f3be7005242f0dbc352a1f0dfc
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2024-11-07 17:31:20 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,6 +1,3 @@
 function solution(my_string) {
-    arr = []
-    let a = [...my_string].map((v) => arr.includes(v) ? '' : arr.push(v))
-    return arr.join('')
-    
+    return [...new Set([...my_string])].join('')
 }
