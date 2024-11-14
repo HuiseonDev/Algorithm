83eebed42bb594c3f1b3fca3673e457237f421c4
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2024-11-14 12:53:42 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 3.03 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,10 +1,7 @@
 function solution(myString, pat) {
     let num= myString.split('').reduce((acc, cur, idx) => 
-             {myString.split('').slice(idx, idx + pat.length).join('') === pat 
-              ? acc.push(myString.split('').slice(idx, idx + pat.length).join('')) : ''
+             {acc.push(myString.split('').slice(idx, idx + pat.length).join('')) 
               return acc}, [])
-    
-    return num.length
-
+   return num.filter(v =>v==pat).length
 
 }
