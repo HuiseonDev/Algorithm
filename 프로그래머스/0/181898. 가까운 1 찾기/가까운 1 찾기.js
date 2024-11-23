500cb483892cc4f045baf1e2b77ed7d48473353d
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2024-11-23 13:45:24 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.71 ms, Memory: 35.1 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,5 +1,4 @@
 function solution(arr, idx) {
-    return arr.findIndex((v, i) => v === 1 && i >= idx) !== -1 ?
-        arr.findIndex((v, i) => v === 1 && i >= idx) : -1
+    return arr.findIndex((v, i) => v === 1 && i >= idx) 
         
 }
