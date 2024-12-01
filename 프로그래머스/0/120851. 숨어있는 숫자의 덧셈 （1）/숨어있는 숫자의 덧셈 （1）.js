3f0150b3ff463b287d1856e7b9f810ea6e8e7cf3
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2024-12-01 18:01:41 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,11 @@
 function solution(my_string) {
-    return my_string.match(/[0-9]/g).map((a) => Number(a)).reduce((a,b) =>(a+b))
-
+//     const regExp = /[0-9]/g;
+//     const answer = my_string.match(regExp)
+//     const array = answer.map((a) => Number(a))
+//     const total = array.reduce((a,b) =>(a+b))
     
 
+//     return total;
+    
+    return my_string.match(/[0-9]/g).map((a) => Number(a)).reduce((a,b) =>(a+b))
 }
