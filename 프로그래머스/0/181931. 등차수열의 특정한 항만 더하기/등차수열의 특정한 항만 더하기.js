283774684b64fc801072fb01169faf3e7bc9f4e9
huiseon Jeon <92114270+HuiseonDev@users.noreply.github.com>
2025-02-04 20:09:09 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,3 +1,3 @@
 function solution(a, d, included) {
-    return included.map((v,i) => v ? a + d * i : '').reduce((a,b) => +a + +b)
+    return included.reduce((acc, flag, i) =>{return flag ? acc + a + d * i : acc}, 0)
 }
